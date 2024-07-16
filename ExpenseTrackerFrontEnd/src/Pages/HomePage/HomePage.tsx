6917af2db7bdeff78ea31d60c0fce9c3f0e49d7a
Bethany Burrows <jedcburrows@gmail.com>
2024-07-16 13:17:56 +0100

    Added styling to home page and changed total to reflect sum of transaction amounts being pulled from db.

--- ExpenseTrackerFrontEnd/src/Pages/HomePage/HomePage.tsx
+++ ExpenseTrackerFrontEnd/src/Pages/HomePage/HomePage.tsx
@@ -7,29 +7,35 @@ type HomePageProps = {
 };
 
 const HomePage = ({ transactions }: HomePageProps) => {
-  console.log(transactions);
+  const calculateTotal = () => {
+    let total: number = 0;
+    transactions.forEach((transaction) => {
+      total += transaction.transaction_amount;
+    });
+    return Math.round(total * 100) / 100;
+  };
 
   return (
     <>
-      <div>This is the home page</div>
-      <h1>£0</h1>
-      <div>
-        <button>Week</button>
-        <button>Month</button>
+      <h1 className="homepage-title">Manage Money</h1>
+      <h5 className="homepage-title"> Total spend:</h5>
+      <div className="homepage-total">
+        <h1 className="homepage-total__number">£{calculateTotal()}</h1>
+        <div className="homepage-total__buttons">
+          <button>Week</button>
+          <button>Month</button>
+        </div>
       </div>
-      <div>
-        <label>List of transactions go here</label>
+      <div className="homepage-transactions">
         {transactions.map((transaction, i) => (
-          <>
-            <div key={i}>
-              <TransactionTab
-                name={transaction.transaction_name}
-                category={transaction.category}
-                cost={transaction.transaction_amount}
-                date={transaction.date}
-              />
-            </div>
-          </>
+          <div key={i} className="homepage-transactions__transaction">
+            <TransactionTab
+              name={transaction.transaction_name}
+              category={transaction.category}
+              cost={transaction.transaction_amount}
+              date={transaction.date}
+            />
+          </div>
         ))}
       </div>
     </>
